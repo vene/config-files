69e021016a97be1626bfea916eb5ffdd613aa1b6
vene <vlad@vene.ro>
2019-02-16 14:40:41 +0000

    some fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,15 @@
-source $HOME/.config/fish/conda.fish
+source (conda info --root)/etc/fish/conf.d/conda.fish
 source $HOME/.config/fish/oceanic.fish
 set -gx TERM xterm-256color
-set -gx PATH /home/vlad/.local/bin /home/vlad/.cargo/bin /home/vlad/conda/bin $PATH
+set -gx PATH /home/vlad/.local/bin /home/vlad/.cargo/bin $PATH
 set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
+
+if status is-interactive
+    abbr --add --global ca conda activate
+    abbr --add --global cda conda deactivate
+    abbr --add --global gca git commit -a
+
+    if not set -q TMUX
+        exec tmux
+    end
+end
